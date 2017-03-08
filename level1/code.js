a50b7923b9b64c897a3b2e139abde56e54fc00c7
MK <mk@macbook-pro-de-mk.home>
2017-03-08 22:17:18 +0100

    Résolution Level 1

--- level1/code.js
+++ level1/code.js
@@ -6,4 +6,43 @@ var carts = data.carts
 
 var result = {};
 
+var cartsArray = [];
+
+// Parcours de chaque cart :
+carts.forEach( function(cart, index) {
+	// console.log('cart :');	
+	// console.log(cart);
+	// console.log('++++++++');
+	var total = 0;
+	var id = cart['id'];
+
+	// Pour chaque cart, parcours de chaque élément de 'items' :
+	cart['items'].forEach( function(item, indx) {
+		// console.log('item :');	
+		// console.log(item);
+		// console.log('++++++++');
+
+		var article_id = item['article_id'];
+		var quantity = item['quantity'];
+
+		//Récupération du prix de l'élément en s'indexant :
+		// Puis multiplication par la quantité
+		var itemPrice = articles[article_id - 1]['price'];
+		var price = itemPrice*quantity;
+
+
+		total += price;
+	});
+
+	cartsArray.push({
+		'id': id,
+		'total': total
+	});
+});
+
+result = {
+	"carts": cartsArray
+};
+
+
 fs.writeFile('output.json', JSON.stringify(result, null, 2));
